3a7de8d12778aee21cc5ff5ca373c468258055a3
Daniel Avalos <daniel_avalos@outlook.com>
2024-11-09 15:13:20 -0600

    refactor App component to use RouterProvider and remove unused state management

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,8 @@
-import { useState, useEffect } from "react";
-import "./App.css";
+import { router } from "./router/Router";
+import { RouterProvider } from "react-router-dom";
 
 function App() {
-  const [isLoading, setIsLoading] = useState(false);
-  const [data, setData] = useState<{ message: string } | null>(null);
-
-  useEffect(() => {
-    setIsLoading(true);
-    fetch("http://localhost:5500/")
-      .then((response) => response.json())
-      .then((data: { message: string }) => {
-        setData(data);
-        console.log(data);
-        setIsLoading(false);
-      });
-  }, []);
-
-  if (isLoading) {
-    return <div className="text-3xl">Loading...</div>;
-  }
-
-  return <h1 className="text-3xl">{data?.message}</h1>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;

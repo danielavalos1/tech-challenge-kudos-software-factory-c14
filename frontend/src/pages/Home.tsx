0c39f997a2b57c5810f95e10969c63d2813ad764
Daniel Avalos <daniel_avalos@outlook.com>
2024-11-09 15:13:39 -0600

    add Home component with basic structure and console log

---
+++ frontend/src/pages/Home.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export const Home = () => {
+  console.log("it works");
+  return (
+    <div>
+      <h1 className="text-3xl">Hello, world!</h1>
+    </div>
+  );
+};

b1a587798deb1e0e8fc19dcdfe4be06e1718f42b
Daniel Avalos <daniel_avalos@outlook.com>
2024-11-09 19:34:53 -0600

    refactor Login component to use useAuth hook and navigate on successful login

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,10 +1,12 @@
 import React, { useState } from "react";
-import { login } from "../services/auth";
 import Form from "../components/Form";
 import { LogIn } from "lucide-react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { useAuth } from "../hooks/useAuth";
 
 export const Login = () => {
+  const { login } = useAuth();
+  const navigate = useNavigate();
   const [formData, setFormData] = useState({
     email: "",
     password: "",
@@ -19,10 +21,9 @@ export const Login = () => {
 
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    console.log(formData);
     try {
-      const response = await login(formData.email, formData.password);
-      console.log(response);
+      await login(formData.email, formData.password);
+      navigate("/");
     } catch (error) {
       console.error(error);
     }
@@ -33,7 +34,7 @@ export const Login = () => {
         <div className="text-center">
           <LogIn className="mx-auto h-12 w-12 text-gray-600 dark:text-gray-400" />
           <h2 className="mt-6 text-3xl font-bold text-gray-900 dark:text-white">
-            Welcome back
+            Data Loading System
           </h2>
           <p className="mt-2 text-sm text-gray-600 dark:text-gray-400">
             Don't have an account?{" "}

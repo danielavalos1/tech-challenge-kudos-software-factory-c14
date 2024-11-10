e0274e3832e8490addb50be5a3a3ebcc779bf369
Daniel Avalos <daniel_avalos@outlook.com>
2024-11-09 19:33:29 -0600

    wrap Home component with Private route for access control

--- frontend/src/router/Router.tsx
+++ frontend/src/router/Router.tsx
@@ -2,11 +2,12 @@ import { createBrowserRouter } from "react-router-dom";
 import { Home } from "../pages/Home";
 import { Login } from "../pages/Login";
 import { Register } from "../pages/Register";
+import { Private } from "../components/Private";
 
 export const router = createBrowserRouter([
   {
     path: "/",
-    element: <Home />,
+    element: <Private children={<Home />} />,
   },
   {
     path: "/login",

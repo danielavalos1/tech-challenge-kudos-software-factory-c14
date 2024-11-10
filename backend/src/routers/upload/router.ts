eae328e5023f685c1c437794022a87462cf3a7c5
Daniel Avalos <daniel_avalos@outlook.com>
2024-11-09 21:58:57 -0600

    add CSV processing functionality to upload route

--- backend/src/routers/upload/router.ts
+++ backend/src/routers/upload/router.ts
@@ -1,11 +1,45 @@
-//create a router
-import { Router, NextFunction, Request, Response } from "express";
+import { Router, Request, Response } from "express";
+import csvParser from "csv-parser";
+import stream from "stream";
+
 const router = Router();
 
-// Routes
+//TODO: despues de obtener los rows, hay que guardar en la base de datos los registros validos y devolver un mensaje con la cantidad de registros guardados
+//TODO: si hay registros invalidos, devolver un mensaje con la cantidad de registros invalidos, y un array con los registros invalidos, indicando el motivo de la invalides
 
-router.post("/", (_req: Request, res: Response, _next: NextFunction) => {
-  res.send("Hello, World!");
+router.post("/", (req: Request, res: Response) => {
+  const csvContent = req.body;
+  if (!csvContent) {
+    res.status(400).json({ message: "No file uploaded" });
+  }
+  try {
+    const results: any[] = [];
+
+    // Crear un flujo de lectura a partir del contenido CSV
+    const readStream = new stream.PassThrough();
+    readStream.end(csvContent);
+
+    // Procesar el CSV utilizando csv-parser
+    readStream
+      .pipe(csvParser())
+      .on("data", (data) => results.push(data))
+      .on("end", () => {
+        // Enviar la respuesta con los datos procesados
+        res.json({
+          message: "Archivo procesado correctamente",
+          data: results,
+        });
+      })
+      .on("error", (error) => {
+        console.error("Error al procesar el archivo CSV", error);
+        res
+          .status(500)
+          .json({ message: "Error al procesar el archivo CSV", error });
+      });
+  } catch (error) {
+    console.error("Error en el servidor", error);
+    res.status(500).json({ message: "Error en el servidor", error });
+  }
 });
 
 export default router;

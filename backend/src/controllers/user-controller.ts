9694e0d596de5f5db44c85a2b2c34c56a7ccf1f7
Daniel Avalos <daniel_avalos@outlook.com>
2024-11-10 20:07:48 -0600

    add saveOne function for single user creation and error handling

--- backend/src/controllers/user-controller.ts
+++ backend/src/controllers/user-controller.ts
@@ -11,6 +11,11 @@ interface SaveUsersResponse {
   errors: Errors[];
 }
 
+interface SaveUserResponse {
+  success: Omit<User, "password">;
+  error?: Detail;
+}
+
 interface Errors {
   row: number;
   details: Detail;
@@ -35,21 +40,9 @@ export async function saveUsers(users: User[]) {
   const failedList: Errors[] = [];
   let successCount = 0;
   let failedCount = 0;
-  const detailKeys: (keyof Detail)[] = [
-    "name",
-    "email",
-    "age",
-    "password",
-    "role",
-  ];
   for (const [index, record] of users.entries()) {
     try {
-      const preparedUser = {
-        ...record,
-        password: "default",
-        age: record.age ? Number(record.age) : null,
-      };
-      const validatedUser = userSchema.parse(preparedUser);
+      const validatedUser = validateUser(record);
       const hashedPassword = await bcrypt.hash(validatedUser.password, 10);
       const user = await UserModel.createUser({
         ...validatedUser,
@@ -60,36 +53,7 @@ export async function saveUsers(users: User[]) {
       successCount++;
     } catch (error: any) {
       failedCount++;
-      let errorMessage = {} as Detail;
-      if (error instanceof ZodError) {
-        error.errors.forEach((err) => {
-          const pathKey = err.path[0];
-          if (
-            typeof pathKey === "string" &&
-            detailKeys.includes(pathKey as keyof Detail)
-          ) {
-            const key = pathKey as keyof Detail;
-            if (!errorMessage[key]) {
-              errorMessage[key] = {
-                value: record[key] as string,
-                error: err.message,
-              };
-            } else {
-              errorMessage[key] = {
-                value: record[key] as string,
-                error: `${errorMessage[key].error}, ${err.message}`,
-              };
-            }
-          }
-        });
-      } else if (error instanceof Error) {
-        if ((error as any).code === "P2002") {
-          errorMessage.email = {
-            value: record.email,
-            error: "Email already exists",
-          };
-        }
-      }
+      const errorMessage = getErrorMessage(error, record);
       failedList.push({ row: index + 1, details: errorMessage, record });
     }
   }
@@ -101,10 +65,78 @@ export async function saveUsers(users: User[]) {
   } as SaveUsersResponse;
 }
 
-export async function getUsers() {
-  return UserModel.getUsers();
+export async function saveOne(record: User) {
+  try {
+    const validatedUser = validateUser(record);
+    const hashedPassword = await bcrypt.hash(validatedUser.password, 10);
+    const user = await UserModel.createUser({
+      ...validatedUser,
+      password: hashedPassword,
+    } as User);
+    const { password, ...userWithoutPassword } = user;
+    return {
+      success: userWithoutPassword,
+    } as SaveUserResponse;
+  } catch (error: any) {
+    const errorMessage = getErrorMessage(error, record);
+    return {
+      success: null,
+      error: errorMessage,
+    };
+  }
+}
+
+function validateUser(user: User) {
+  const preparedUser = {
+    ...user,
+    password: user.password || "default",
+    age: user.age ? Number(user.age) : null,
+  };
+  return userSchema.parse(preparedUser);
+}
+
+function getErrorMessage(error: ZodError | Error, record: User) {
+  const detailKeys: (keyof Detail)[] = [
+    "name",
+    "email",
+    "age",
+    "password",
+    "role",
+  ];
+  let errorMessage = {} as Detail;
+  if (error instanceof ZodError) {
+    error.errors.forEach((err) => {
+      const pathKey = err.path[0];
+      if (
+        typeof pathKey === "string" &&
+        detailKeys.includes(pathKey as keyof Detail)
+      ) {
+        const key = pathKey as keyof Detail;
+        if (!errorMessage[key]) {
+          errorMessage[key] = {
+            value: record[key] as string,
+            error: err.message,
+          };
+        } else {
+          errorMessage[key] = {
+            value: record[key] as string,
+            error: `${errorMessage[key].error}, ${err.message}`,
+          };
+        }
+      }
+    });
+  } else if (error instanceof Error) {
+    if ((error as any).code === "P2002") {
+      errorMessage.email = {
+        value: record.email,
+        error: "Email already exists",
+      };
+    }
+  }
+  return errorMessage;
 }
 
 export default {
   saveUsers,
+  saveOne,
 };
